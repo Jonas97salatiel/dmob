94c31f4a555b2114c46453b907267e80bdc061b8
Jonas Salatiel <jonas97salatiel@gmail.com>
2020-03-26 03:59:54 -0300

    fazendo lista3

---
+++ atividade3/ex1/ex1.js
@@ -0,0 +1,28 @@
+function corAmarelo(){
+    var body = document.getElementById("body");
+
+    body.style.background = "yellow"
+
+}
+
+function corVermelho(){
+    var body = document.getElementById("body");
+
+    body.style.background = "red"
+
+}
+
+function corVerde(){
+    var body = document.getElementById("body");
+
+    body.style.background = "green"
+
+}
+
+function corAzul(){
+    var body = document.getElementById("body");
+
+    body.style.background = "blue"
+
+}
+
