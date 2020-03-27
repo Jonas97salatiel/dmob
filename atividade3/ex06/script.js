c3a61e554a6f66de0290db89c47389b2910e40b2
Jonas Salatiel <jonas97salatiel@gmail.com>
2020-03-26 23:21:49 -0300

    fazendo lista3

--- atividade3/ex06/script.js
+++ atividade3/ex06/script.js
@@ -1,1 +1,12 @@
+function palavraPolindroma(){
 
+var palavra = document.getElementById("palavra").value;
+
+if (palavra == palavra.split('').reverse().join('')) {
+   alert(palavra + ' é uma palavra polindroma.');
+ } else {
+   alert(palavra + ' não é uma palavra polindroma');
+  }
+
+
+}
