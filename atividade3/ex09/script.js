f17fb59c12c74821803c4e8d58771bff87ceff42
Jonas Salatiel <jonas97salatiel@gmail.com>
2020-03-31 20:20:39 -0300

    Ajustes no ex 09

--- atividade3/ex09/script.js
+++ atividade3/ex09/script.js
@@ -1,39 +1,44 @@
-function juntarNomes(e){
-  e.preventDefalt();
+function juntarNomes(){
   
-  var nome1 = document.getElementById("1").value;
-  var nome2 = document.getElementById("2").value;
-  var nome3 =[];
-  cont1 = 0;
+  var form = document.querySelector('form');
+
+  form.addEventListener('submit', function( event ) {
+    event.preventDefault();
+    
+    var nome1 = document.getElementById("1").value;
+    var nome2 = document.getElementById("2").value;
+    var nome3 =[];
+    cont1 = 0;
+    
   
-
-  if(nome1.length > nome2.length){
-
-    for (cont = 0; cont <= nome1.length; cont++){
-
-      
-      nome3[cont1] = nome1[cont]
-      cont1 ++
-      nome3[cont1] = nome2[cont]
-      cont1 ++
+    if(nome1.length > nome2.length){
+  
+      for (cont = 0; cont <= nome1.length; cont++){
+  
+        
+        nome3[cont1] = nome1[cont]
+        cont1 ++
+        nome3[cont1] = nome2[cont]
+        cont1 ++
+      }
+  
+    }else{
+  
+      for (cont = 0; cont <= nome2.length; cont++){
+  
+        
+        nome3[cont1] = nome1[cont]
+        cont1 ++
+        nome3[cont1] = nome2[cont]
+        cont1 ++
+      }
+  
     }
+    console.log(nome3.join(''));
 
-  }else{
-
-    for (cont = 0; cont <= nome2.length; cont++){
-
-      
-      nome3[cont1] = nome1[cont]
-      cont1 ++
-      nome3[cont1] = nome2[cont]
-      cont1 ++
-    }
-
-  }
-
-  document.getElementById('3').value = nome3.join('');
-
+    document.getElementById('3').value = nome3.join('');
   
+  });
 
 
 }
